dcb58f4a3f008bc4b30a86833ad9d54e6aed2025
Fedor Borshev <fedor@borshev.com>
2020-02-22 12:12:38 +0300

    Final slide changes

--- src/components/LinkSlide.vue
+++ src/components/LinkSlide.vue
@@ -2,12 +2,15 @@
   <section>
     <h2>Задаём вопросы</h2>
     <img class="link__qr-code" src="@/assets/qr-code.png" />
-    <h3>borshev.com/q</h3>
+    <h3 class="link__link">borshev.com/q</h3>
   </section>
 </template>
 
 <style>
 .link__qr-code {
   height: 50vh;
+}
+.link__link {
+  text-transform: none !important;
 }
 </style>
--- src/components/PlanSlide.vue
+++ src/components/PlanSlide.vue
@@ -1,10 +1,34 @@
 <template>
   <section>
     <h2>План</h2>
-    <ul>
-      <li>40 минут говорим</li>
-      <li>10 минут перерыв</li>
-      <li>Продолжаем</li>
-    </ul>
+    <div class="plan__container">
+      <div class="plan__qr-code">
+        <img class="plan__img" src="@/assets/qr-code.png">
+      </div>
+      <div class="plan__plan">
+        <ul>
+          <li>Говорим до 15:15</li>
+          <li>Перерыв 10 минут</li>
+          <li>Говорим до 16:30</li>
+        </ul>
+        <p class="plan__waiting fragment">Ждём вопросов :-)</p>
+      </div>
+    </div>
   </section>
 </template>
+
+<style>
+.plan__container {
+  display: flex;
+}
+.plan__qr-code,
+.plan__plan {
+  flex: 1;
+}
+.plan__img {
+  height: 40vh;
+}
+.plan__weight {
+  font-weight: bold;
+}
+</style>
